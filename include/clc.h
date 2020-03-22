a1b62077faeb7b1661a8067e084123398f1d8eaf
oficsu <oficsu@gmail.com>
2020-03-22 03:57:40 +0300

    GeneratorID was replaced with type template parameter

--- include/clc.h
+++ include/clc.h
@@ -21,17 +21,17 @@ namespace clc
     namespace detail
     {
         template<
-            std::size_t GeneratorID,
+            typename Tag,
             std::size_t V>
         struct declarator {
             enum {
                 value = V
             };
-            friend constexpr std::size_t get(declarator<GeneratorID, V>);
+            friend constexpr std::size_t get(declarator<Tag, V>);
         };
 
         template<
-            std::size_t GeneratorID,
+            typename Tag,
             typename Src>
         struct infector {
             friend constexpr std::size_t get(Src) {
@@ -40,33 +40,36 @@ namespace clc
         };
 
         template<
-            std::size_t GeneratorID,
+            typename Tag,
             std::size_t Index = 0>
         constexpr auto up_to_one(float k) -> std::size_t {
-            using t = decltype(infector<GeneratorID, declarator<GeneratorID, Index>>{});
-            return get(declarator<GeneratorID, Index>()) + static_cast<std::size_t>(k);
+            using t = decltype(infector<Tag, declarator<Tag, Index>>{});
+            return get(declarator<Tag, Index>()) + static_cast<std::size_t>(k);
         };
 
         template<
-            std::size_t GeneratorID,
+            typename Tag,
             std::size_t Index = 0,
-            std::size_t Next = get(declarator<GeneratorID, Index>{})>
+            std::size_t Next = get(declarator<Tag, Index>{})>
         constexpr std::size_t up_to_one(int i) {
-            return up_to_one<GeneratorID, Next>(i);
+            return up_to_one<Tag, Next>(i);
         };
+
+        template<std::size_t Number>
+        struct numeric_tag { enum { value = Number }; };
 
         #ifndef CURIOUS_LOOPHOLED_COUNTER_LIMIT
             #define CURIOUS_LOOPHOLED_COUNTER_LIMIT 45
         #endif
 
         #define CURIOUS_LOOPHOLED_COUNTER_SUM(z, n, data)  + _##n
-        #define CURIOUS_LOOPHOLED_COUNTER_UP_TO_ONE_SEQUENCE(z, n, data) std::size_t _##n = up_to_one<0+ BOOST_PP_REPEAT(n, CURIOUS_LOOPHOLED_COUNTER_SUM,)>(n),
+        #define CURIOUS_LOOPHOLED_COUNTER_UP_TO_ONE_SEQUENCE(z, n, data) std::size_t _##n = up_to_one<numeric_tag<0+ BOOST_PP_REPEAT(n, CURIOUS_LOOPHOLED_COUNTER_SUM,)>>(n),
         #define CURIOUS_LOOPHOLED_COUNTER_SEQUENCE(n) BOOST_PP_REPEAT_FROM_TO(1, n, CURIOUS_LOOPHOLED_COUNTER_UP_TO_ONE_SEQUENCE, 1)
 
         constexpr std::size_t offset = ((2 + CURIOUS_LOOPHOLED_COUNTER_LIMIT - 1) * CURIOUS_LOOPHOLED_COUNTER_LIMIT) / 2 + 1;
 
         template<
-            std::size_t _0 = up_to_one<0>(1),
+            std::size_t _0 = up_to_one<numeric_tag<0>>(1),
             CURIOUS_LOOPHOLED_COUNTER_SEQUENCE(CURIOUS_LOOPHOLED_COUNTER_LIMIT)
             std::size_t = 0>
         constexpr std::size_t up_to_limit() {
