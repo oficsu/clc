eb3125fdd0a77c14038912d025e0a61f5a58da66
oficsu <oficsu@gmail.com>
2021-11-10 22:00:00 +0300

    Rewrite code to use optimized version for GCC
    Almost all entities have been renamed
    Many redundant things have been removed

--- include/clc.h
+++ include/clc.h
@@ -17,145 +17,212 @@
 
 namespace clc
 {
-    namespace detail
+    namespace meta
     {
         enum class unit { value };
+    }
 
-        template<typename Tag>
-        struct declarator {
-            friend constexpr unit get(declarator);
-        };
+    namespace detail
+    {
+        namespace type_loophole
+        {
+            template<typename Tag>
+            struct flag {
+                friend constexpr meta::unit get(flag);
+            };
 
-        template<typename Tag, typename Declarator>
-        struct infector {
-            friend constexpr unit get(Declarator) {
-                return unit::value;
-            }
-        };
+            template<typename Tag, typename Flag>
+            struct writer {
+                friend constexpr meta::unit get(Flag) {
+                    return meta::unit::value;
+                }
+            };
 
-        template<typename Tag>
-        constexpr bool toggle_impl(float) {
-            return sizeof(infector<Tag, declarator<Tag>>);
-        };
+            template<typename Tag, bool = true>
+            constexpr bool toggle_impl(...) {
+                return sizeof(writer<Tag, flag<Tag>>);
+            };
 
-        template<typename Tag, unit = get(declarator<Tag>{})>
-        constexpr bool toggle_impl(int) {
-            return false;
-        };
+            template<typename Tag, bool = true, meta::unit = get(flag<Tag>{})>
+            constexpr bool toggle_impl(meta::unit) {
+                return false;
+            };
+        }
 
+        template<typename Tag, bool Value = type_loophole::toggle_impl<Tag>(meta::unit::value)>
+        static constexpr bool toggle() { return Value; }
+    }
+
+    namespace meta
+    {
         template<std::size_t Value>
         using index = std::integral_constant<std::size_t, Value>;
 
-        template<bool Value>
-        using boolean = std::integral_constant<bool, Value>;
-
-        template<typename Tag, typename Value = boolean<toggle_impl<Tag>(42)>>
-        using toggle = Value;
-
-        template<bool C>
-        struct partial_conditional;
-
-        template<>
-        struct partial_conditional<true> {
-            template<template<typename...> class Then, typename Else, typename ...Args> using type = Then<Args...>;
-        };
-
-        template<>
-        struct partial_conditional<false> {
-            template<template<typename...> class Then, typename Else, typename ...Args> using type = Else;
-        };
-
-        template<bool C, template<typename...> class Then, typename Else, typename ...Args>
-        using partial_conditional_t = typename partial_conditional<C>:: template type<Then, Else, Args...>;
-
-        template<std::size_t Value, typename>
-        struct recursive_tag : index<Value + 1> {};
-
-        struct force_true {};
-
-        static_assert(toggle<force_true>() == 1, "");
-        static_assert(toggle<force_true>() == 0, "");
-
+        // base case of recursion
         struct bit {
             enum { max = 1 };
 
-            template<
-                typename Tag,
-                typename Freezed = boolean<false>,
-                #if defined(__GNUG__)
-                    std::size_t Value = toggle<typename std::conditional<Freezed::value, force_true, recursive_tag<0, Tag>>::type>::value
-                #else
-                    std::size_t Value = partial_conditional_t<!Freezed::value, toggle, index<1>, recursive_tag<0, Tag>>::value
-                #endif
-            >
-            using next = index<Freezed::value ? 1 : Value>;
+            template<typename Tag, bool Value = detail::toggle<Tag>()>
+            using next = index<Value>;
         };
 
+
+
+        template<bool Cond, typename Tag>
+        using conditionally_hide_tag = typename std::enable_if<Cond, Tag>::type;
+
+        template<
+            bool Cond,
+            typename Bits,
+            typename Tag,
+            typename..., // private variables:
+                typename ConditionalTag = conditionally_hide_tag<Cond, Tag>,
+                std::size_t R = Bits:: template next<ConditionalTag>::value>
+        static constexpr std::size_t conditional_next_for(meta::unit) {
+            return R;
+        }
+
+        template<bool Cond, typename Bits, typename Tag>
+        static constexpr std::size_t conditional_next_for(...) {
+            return Bits::max;
+        }
+
+        template<
+            typename Bits,
+            typename Tag,
+            typename..., // private variable:
+                std::size_t R = Bits:: template next<Tag>::value
+        >
+        static constexpr std::size_t unconditional_next_for() {
+            return R;
+        }
+
+        template<typename>
+        struct left_recursive_tag {};
+
+        template<typename>
+        struct right_recursive_tag {};
+
+
+
+        // recursive case
         template<typename Nested = bit>
         struct add_bit {
-            enum { max = Nested::max * 2 };
-
-            template<typename Args, typename Freezed>
-            using previous = typename Nested:: template next<Args, Freezed>;
-
-            template<bool Freezed, typename Tag>
-            using instantiate_for_false_only = partial_conditional_t<!Freezed, previous, index<Nested::max>, Tag, boolean<Freezed>>;
+            enum : std::size_t { max = Nested::max * 2 };
 
             template<
                 typename Tag,
-                typename Freezed = boolean<false>,
-                #if defined(__GNUG__)
-                    std::size_t First  = Nested:: template next<recursive_tag<0, Tag>, Freezed>::value, // instantiate_for_false_only<Freezed::value, recursive_tag<0, Tag>>::value,
-                    std::size_t Second = Nested:: template next<recursive_tag<1, Tag>, boolean<!!First>>::value
-                #else
-                    std::size_t First  = instantiate_for_false_only<Freezed::value, recursive_tag<0, Tag>>::value,
-                    std::size_t Second = instantiate_for_false_only<bool ( First ), recursive_tag<1, Tag>>::value
-                #endif
+                // private variables:
+                std::size_t First  = unconditional_next_for<Nested, left_recursive_tag<Tag>>(),
+                std::size_t Second = conditional_next_for<!First, Nested, right_recursive_tag<Tag>>(meta::unit::value)
             >
             using next = index<First + Second>;
         };
 
+
+
+        namespace detail
+        {
+            template<std::size_t Width>
+            struct bits {
+                using type = add_bit<typename bits<Width - 1>::type>;
+            };
+
+            template<>
+            struct bits<1> {
+                using type = add_bit<>;
+            };
+        }
+
         template<std::size_t Width>
-        struct bits : add_bit<bits<Width - 1>> {};
+        using bits = typename detail::bits<Width>::type;
 
-        template<>
-        struct bits<0> : bit {};
 
-        template<typename Nested>
-        struct reverse {
-            template<
-                typename Tag,
-                typename Freezed = boolean<false>,
-                std::size_t Value = Nested:: template next<Tag, Freezed>::value
-            >
-            using next = index<Nested::max - Value>;
-        };
 
         template <std::size_t Value>
         struct log2 : index<1 + log2<Value / 2>::value> {};
 
         template<> struct log2<1> : index<1> {};
+
+
+
+        static constexpr std::size_t max(std::size_t lhs, std::size_t rhs) {
+            return lhs > rhs ? lhs : rhs;
+        }
+        static constexpr std::size_t min(std::size_t lhs, std::size_t rhs) {
+            return lhs < rhs ? lhs : rhs;
+        }
+        static constexpr std::size_t diff(std::size_t lhs, std::size_t rhs) {
+            return (lhs > rhs ? lhs - rhs : rhs - lhs);
+        }
     }
 
+
+
     struct default_counter_tag {};
+
+    template<std::size_t, std::size_t>
+    struct default_range_counter_tag {};
+
     struct default_reverse_counter_tag {};
+
+
+
+    // instantiated fast enough on Clang (on my laptop it stucks at ~ 44)
+    // and on GCC (on my laptop it stucks at ~ 75, much more than necessary)
+    enum { default_counter_width = 32 };
+
+
 
     template<
         typename Tag = default_counter_tag,
-        std::size_t value = detail::reverse<detail::bits<16>>::next<Tag>()
+        std::size_t Width = default_counter_width,
+
+        typename..., // private variables:
+            typename Bits = meta::bits<Width>,
+            std::size_t Index = Bits::template next<Tag>::value
     >
-    constexpr std::size_t counter() { return value; }
+    constexpr std::size_t counter() {
+        static_assert (Bits::max >= Index, "counter overflow");
+        return Bits::max - Index;
+    }
+
+
 
     template<
         std::size_t InitialValue,
         typename Tag = default_reverse_counter_tag,
-        typename BitWidth = detail::log2<InitialValue - 1>,
-        typename Bits = typename detail::bits<BitWidth::value>,
-        typename Index = typename Bits::template next<Tag>
+
+        typename..., // private variables:
+            typename Width = meta::log2<InitialValue - 1>,
+            std::size_t Index = counter<Tag, Width::value>()
     >
     constexpr std::size_t reverse_counter() {
-        static_assert ((Index::value - (Bits::max - InitialValue)) <= InitialValue, "Overflow");
-        return Index::value - (Bits::max - InitialValue);
+        static_assert (Index >= 0, "counter underflow");
+        return InitialValue - Index;
+    }
+
+
+
+    template<
+        std::size_t Begin,
+        std::size_t End,
+        typename Tag = default_range_counter_tag<Begin, End>,
+
+        typename..., // private variables:
+            bool IsDirect = (Begin < End),
+            std::size_t RangeSize = meta::diff(Begin, End),
+            std::size_t Index = reverse_counter<RangeSize, Tag>(),
+            std::size_t Min = meta::min(Begin, End),
+            std::size_t Max = meta::max(Begin, End)
+    >
+    constexpr std::size_t range_counter() {
+            // we can use single "counter out of range" statement,
+            // but this message less useful for the user
+            static_assert (Max - Index <= Max || ! IsDirect, "counter overflow");
+            static_assert (Max - Index <= Max ||   IsDirect, "counter underflow");
+            return IsDirect ? Max - Index
+                            : Min + Index;
     }
 }
 
