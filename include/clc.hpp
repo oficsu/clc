e03485495f6e55dfc6a54c0d4b6bc33d1d145471
oficsu <oficsu@gmail.com>
2021-11-16 14:00:00 +0300

    Minor fixes
    Include <type_traits> instead <utility>
    Fix '// private variable(s)' comments
    Don't place '>' on a new line
    Add license to assign.cpp

--- include/clc.hpp
+++ include/clc.hpp
@@ -12,7 +12,7 @@
 #ifndef CURIOUS_LOOPHOLED_COUNTER_HPP
 #define CURIOUS_LOOPHOLED_COUNTER_HPP
 
-#include <utility>
+#include <type_traits>
 
 #ifdef __GNUC__
     #pragma GCC diagnostic push
@@ -63,7 +63,10 @@ namespace clc
                 return false;
             };
 
-            template<typename Tag, typename..., meta::unit = get(flag<Tag>{})>
+            template<
+                typename Tag,
+                typename..., // private variable:
+                    meta::unit = get(flag<Tag>{})>
             static constexpr bool read(meta::unit) {
                 return true;
             };
@@ -73,25 +76,37 @@ namespace clc
                 return !sizeof(writer<Tag, flag<Tag>>);
             };
 
-            template<typename Tag, typename..., meta::unit = get(flag<Tag>{})>
+            template<
+                typename Tag,
+                typename..., // private variable:
+                    meta::unit = get(flag<Tag>{})>
             static constexpr bool toggle_impl(meta::unit) {
                 return true;
             };
         }
 
-        template<typename Tag, typename..., bool Result = type_loophole::read<Tag>(meta::unit::value)>
+        template<
+            typename Tag,
+            typename..., // private variable:
+                bool Result = type_loophole::read<Tag>(meta::unit::value)>
         static constexpr bool read() {
             return Result;
         };
 
-        template<typename Tag, typename..., bool Result = type_loophole::toggle_impl<Tag>(meta::unit::value)>
+        template<
+            typename Tag,
+            typename..., // private variable:
+                bool Result = type_loophole::toggle_impl<Tag>(meta::unit::value)>
         static constexpr bool toggle() { return Result; }
 
         // why toggle rather than something
         // like a non-overloaded type_loophole::set?
         //
         // consult with clang        '"\_( ''_ )_/"'
-        template<typename Tag, typename..., bool = toggle<Tag>()>
+        template<
+            typename Tag,
+            typename..., // private variable:
+                bool = toggle<Tag>()>
         static constexpr meta::unit set() {
             return meta::unit::value;
         };
@@ -113,9 +128,8 @@ namespace clc
                 template<
                     std::size_t Pivot,
                     typename Tag,
-                    typename..., // private variables:
-                        bool IsIndexReached = detail::read<index<Tag, Pivot>>()
-                >
+                    typename..., // private variable:
+                        bool IsIndexReached = detail::read<index<Tag, Pivot>>()>
                 static constexpr std::size_t next() {
                     return Pivot + IsIndexReached;
                 }
@@ -133,8 +147,7 @@ namespace clc
                         bool IsIndexReached = detail::read<index<Tag, Pivot>>(),
                         std::size_t Shift = NextSize / 2 + 1,
                         std::size_t NextPivot = IsIndexReached ? Pivot + Shift : Pivot - Shift,
-                        std::size_t Result = binary_searcher<NextSize>::template next<NextPivot, Tag>()
-                >
+                        std::size_t Result = binary_searcher<NextSize>::template next<NextPivot, Tag>()>
                 static constexpr std::size_t next() {
                     return Result;
                 }
@@ -201,12 +214,12 @@ namespace clc
 
 
 
+
     template<
         typename Tag,
         std::size_t Size = static_cast<std::size_t>(-1),
         typename..., // private variable:
-            std::size_t Result = meta::binary_searcher<Size>::template next<Size / 2, Tag>()
-    >
+            std::size_t Result = meta::binary_searcher<Size>::template next<Size / 2, Tag>()>
     static constexpr std::size_t search() {
         static_assert (
             meta::is_mersenne_number(Size),
@@ -220,7 +233,7 @@ namespace clc
     template<
         typename Tag,
         std::size_t Value,
-        typename...,
+        typename..., // private variable:
             meta::unit = meta::binary_assigner<Value>::template next<Tag>()>
     static constexpr meta::unit assign() {
         return meta::unit::value;
@@ -239,14 +252,22 @@ namespace clc
         typename..., // private variables:
             std::size_t Size = meta::round_up_to_mersenne_number(UpTo),
             std::size_t Index = search<Tag, Size>(),
-            bool IsIndexReachedEarlier = detail::toggle<meta::index<Tag, Index>>()
-    >
+            bool IsIndexReachedEarlier = detail::toggle<meta::index<Tag, Index>>()>
     static constexpr std::size_t counter() {
         static_assert(
             Index <= UpTo && !IsIndexReachedEarlier,
             "counter overflow detected");
 
         return Index;
+    }
+
+    template<
+        typename Tag,
+        std::size_t UpTo = default_counter_size,
+        typename..., // private variable:
+            std::size_t Result = counter<UpTo, Tag>()>
+    static constexpr std::size_t counter() {
+        return Result;
     }
 
 
@@ -261,8 +282,7 @@ namespace clc
             std::size_t Size = meta::round_up_to_mersenne_number(DownTo),
             std::size_t Index = search<Tag, Size>(),
             bool IsIndexReachedEarlier = detail::toggle<meta::index<Tag, Index>>(),
-            std::size_t Result = DownTo - Index
-    >
+            std::size_t Result = DownTo - Index>
     static constexpr std::size_t reverse_counter() {
         static_assert(
             Result <= DownTo && !IsIndexReachedEarlier,
@@ -284,8 +304,7 @@ namespace clc
             typename ConditionalTag = meta::enable_tag_if<(Begin < End), Tag>,
             std::size_t Size = End - Begin,
             std::size_t Index = counter<Size, ConditionalTag>(),
-            typename /* fixes overload resolution */ = void
-    >
+            typename /* fixes overload resolution */ = void>
     static constexpr std::size_t range_counter() {
         return Begin + Index;
     }
@@ -297,8 +316,7 @@ namespace clc
         typename..., // private variables:
             typename ConditionalTag = meta::enable_tag_if<(Begin >= End), Tag>,
             std::size_t Size = Begin - End,
-            std::size_t Index = reverse_counter<Size, ConditionalTag>()
-    >
+            std::size_t Index = reverse_counter<Size, ConditionalTag>()>
     static constexpr std::size_t range_counter() {
         return End + Index;
     }
--- tests/sources/assign.cpp
+++ tests/sources/assign.cpp
@@ -1,3 +1,12 @@
+//                 Curious Loopholed Counter
+//
+//                Copyright Ofee Oficsu 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//     (See accompanying file LICENSE_1_0.txt or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
+//
+//        Project home: https://github.com/oficsu/clc
+
 #include <clc.hpp>
 
 static constexpr auto unit = clc::meta::unit::value;
